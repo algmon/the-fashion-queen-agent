0a2ad18b0198f834b48a8538b19b424f613f2e83
W <124419632+weijiang2023@users.noreply.github.com>
2025-02-03 22:52:33 +0800

    Update play.py

--- play.py
+++ play.py
@@ -9,110 +9,130 @@ THEORY_2 = "Probability"
 THEORY_3 = "AI Agent"
 THEORY_4 = "Entropy"
 
-class world:
+class World:
     # core variables
-    provider = ""
-    intelligence = 0
-    sub_class = "env"
-    geo_location_region = "north america"
-    complex_level = 0
-    time = 0
-    time_for_each_play = 0
-    space = 0
-    things = 0
-    knowledge = 0
-    energy = 0
-    energy_for_each_play = 0
-    security_provider = ""
-    security = 0
-    privacy_provider = ""
-    privacy = 0
-    history = 0
+    def __init__(self):
+        self.provider = ""
+        self.intelligence = 0
+        self.sub_class = "env"
+        self.geo_location_region = "north america"
+        self.complex_level = 0
+        self.time = 0
+        self.time_for_each_play = 0
+        self.space = 0
+        self.things = 0
+        self.knowledge = 0
+        self.energy = 0
+        self.energy_for_each_play = 0
+        self.security_provider = ""
+        self.security = 0
+        self.privacy_provider = ""
+        self.privacy = 0
+        self.history = 0
+
     # core functions
-    def reward():
-        pass
-    def punish():
-        pass
-    def give():
-        pass
-    def take():
+    def reward(self):
         pass
 
-class agent:
+    def punish(self):
+        pass
+
+    def give(self):
+        pass
+
+    def take(self):
+        pass
+
+class Agent:
     # core variables
-    autopilot_provider = "Suanfamama"
-    autopilot = 0
-    brain_provider = "Suanfamama"
-    body_provider = "Suanfamama"
-    designed_for = ["Suanfamama_beauty_capture_tasks"]
-    intelligence = 0
-    sub_class_1 = "red" # red or blue
-    sub_class_2 = "robot"
-    current_task = "capture the beauty"
-    tasks_waiting_list = ["wash the dishes", "clean the house", "post a moment"]
-    states = [] # unified graph based action and state dynamic map with probability computation in real time
-    actions = []
-    solver_provider = "Suanfamama"
-    solver = 0
-    nlp_provider = "Suanfamama" # for natural language processing - mostly language translation
-    nlp = 0
-    cv_provider = "Suanfamama"
-    cv = 0
+    def __init__(self):
+        self.autopilot_provider = "Suanfamama"
+        self.autopilot = 0
+        self.brain_provider = "Suanfamama"
+        self.body_provider = "Suanfamama"
+        self.designed_for = ["Suanfamama_beauty_capture_tasks"]
+        self.intelligence = 0
+        self.sub_class_1 = "red" # red or blue
+        self.sub_class_2 = "robot"
+        self.current_task = "capture the beauty"
+        self.tasks_waiting_list = ["wash the dishes", "clean the house", "post a moment"]
+        self.states = [] # unified graph based action and state dynamic map with probability computation in real time
+        self.actions = []
+        self.solver_provider = "Suanfamama"
+        self.solver = 0
+        self.nlp_provider = "Suanfamama" # for natural language processing - mostly language translation
+        self.nlp = 0
+        self.cv_provider = "Suanfamama"
+        self.cv = 0
+        self.perception_provider = "Suanfamama"
+        self.perception = 0
+        self.planner_provider = "Suanfamama"
+        self.planning = 0
+        self.reasoning_provider = "Suanfamama"
+        self.reasoning = 0
+        self.action_provider = "Suanfamama"
+        self.action = 0
+        self.search_provider = "Suanfamama"
+        self.search = 0
+        self.capability = 0
+        self.responsibility = 0
+        self.knowledge_provider = "Suanfamama"
+        self.knowledge = 0
+        self.energy_provider = "Suanfamama"
+        self.energy = 0
+        self.money_provider = "Suanfamama"
+        self.money = 0
+        self.time_provider = "Suanfamama"
+        self.time = 0
+        self.memory_provider = "Suanfamama"
+        self.memory = 0
+        self.protect_provider = "Suanfamama"
+        self.protect = 0
+        self.friends = 0
+        self.enemies = 0
+        self.family = 0
+        self.history_provider = "Suanfamama"
+        self.history = 0
+        self.security_provider = "Suanfamama"
+        self.security = 1
+        self.privacy_provider = "Suanfamama"
+        self.privacy = 0
+        self.safety_provider = "Suanfamama"
+        self.safety = 0
 
-    perception_provider = "Suanfamama"
-    perception = 0
-    planner_provider = "Suanfamama"
-    planning = 0
-    reasoning_provider = "Suanfamama"
-    reasoning = 0
-    action_provider = "Suanfamama"
-    action = 0
-
-    search_provider = "Suanfamama"
-    search = 0
-    capability = 0
-    responsibility = 0
-    knowledge_provider = "Suanfamama"
-    knowledge = 0
-    energy_provider = "Suanfamama"
-    energy = 0
-    money = 0
-    time = 0
-    memory = 0
-    #plan = 0
-    #reason = 0
-    #action = 0
-    protect_provider = "Suanfamama"
-    protect = 0
-    friends = 0
-    enemies = 0
-    family = 0
-    history_provider = "Suanfamama"
-    history = 0
-    security_provider = "Suanfamama"
-    security = 1
-    privacy_provider = "Suanfamama"
-    privacy = 1
-    privacy_provider = "Suanfamama"
-    privacy = 0
-    safety_provider = "Suanfamama"
-    safety = 0
     # core functions
-    def meditate():
+    def meditate(self):
         pass
-    def move():
+
+    def move(self):
         pass
-    def think():
+
+    def think(self):
         pass
-    def capture():
+
+    def capture(self):
         pass
-    def live():
+
+    def live(self):
         pass
-    def work():
+
+    def work(self):
         pass
-    def learn():
+
+    def learn(self):
         pass
-    def study():
+
+    def study(self):
         pass
-    def transfer():
+
+    def transfer(self):
         pass
+
+    def distill(self):
+        pass
+
+class FashionAgent(Agent):
+    pass
+
+class EducationAgent(Agent):
+    pass
